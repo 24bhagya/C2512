c7af67f754005d247d9e86c0f0bf85d8a0f5e54d
Bhagya S Kumar <57554630+24bhagya@users.noreply.github.com>
2024-12-01 21:57:22 +0530

    Created surgery.cpp

---
+++ Phase1/C2512/Day16/task/surgery.cpp
@@ -0,0 +1,37 @@
+#include "Surgery.h"
+
+Surgery::Surgery(string p_SurgeryID, int p_Duration)
+{
+    SurgeryID = p_SurgeryID;
+    Duration = p_Duration;
+}
+
+bool Surgery::Equals(const Surgery& other)
+{
+    return (Duration == other.Duration);
+}
+
+bool Surgery::NotEquals(const Surgery& other)
+{
+    return (Duration != other.Duration);
+}
+
+bool Surgery::GreaterThan(const Surgery& other)
+{
+    return (Duration > other.Duration);
+}
+
+bool Surgery::GreaterThanEquals(const Surgery& other)
+{
+    return (Duration >= other.Duration);
+}
+
+bool Surgery::LessThan(const Surgery& other)
+{
+    return (Duration < other.Duration);
+}
+
+bool Surgery::LessThanEquals(const Surgery& other)
+{
+    return (Duration <= other.Duration);
+}
