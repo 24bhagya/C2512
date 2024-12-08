dc98be371d7e1de62b1cfd2ab60f26d7710c41a6
Bhagya S Kumar <57554630+24bhagya@users.noreply.github.com>
2024-12-08 23:45:47 +0530

    Created task2.cpp

---
+++ Phase1/C2512/Day23/task2.cpp
@@ -0,0 +1,74 @@
+//Example 9: `Gaming System`
+//`Base Class:` Character `{field: health, behavior: move()}`
+//- `Derived Class:` Warrior `{field: weaponType, behavior: attack()}`
+
+#include <iostream>
+#include <string>
+
+using  std::cout;
+using std::endl;
+using std::string;
+
+// Base Class
+class Character {
+protected:
+    int health;
+
+public:
+    Character(int h) : health(h) {
+        cout << "Character constructor called. Health: " << health << endl;
+    }
+
+    ~Character() {
+        cout << "Character destructor called.\n";
+    }
+
+    void move() {
+        cout << "Character is moving. Health: " << health << endl;
+    }
+};
+
+// Derived Class
+class Warrior : public Character {
+private:
+    string weaponType;
+
+public:
+    Warrior(int h, const string& weapon) : Character(h), weaponType(weapon) {
+        cout << "Warrior constructor called. Weapon: " << weaponType << endl;
+    }
+
+    ~Warrior() {
+        cout << "Warrior destructor called.\n";
+    }
+
+    void attack() {
+        cout << "Warrior is attacking with " << weaponType << ". Health: " << health << endl;
+    }
+};
+
+int main() {
+    // Creating static objects
+    cout << "Creating static objects:\n";
+    Character staticCharacter(100);
+    Warrior staticWarrior(200, "Sword");
+
+    staticCharacter.move();
+    staticWarrior.move();
+    staticWarrior.attack();
+
+    cout << "\nCreating dynamic objects:\n";
+    // Creating dynamic objects
+    Character* dynamicCharacter = new Character(80);
+    Warrior* dynamicWarrior = new Warrior(150, "Axe");
+
+    dynamicCharacter->move();
+    dynamicWarrior->move();
+    dynamicWarrior->attack();
+
+    cout << "\nDeleting dynamic objects:\n";
+    delete dynamicCharacter;
+    delete dynamicWarrior;
+
+    return 0;
+}
