91c3765799d5f05cdb095ef1a512cdbb7a9ee746
Bhagya S Kumar <57554630+24bhagya@users.noreply.github.com>
2024-12-17 15:53:19 +0530

    Update p01.cpp

--- Phase1/C2512/Evaluation 3/p01.cpp
+++ Phase1/C2512/Evaluation 3/p01.cpp
@@ -1,44 +1,51 @@
-// Coding Question: 1.1
-/*
-    Employee {id, age, name} //id, age, name are value objects 
-    Employee e1(101,22,"Athira"),e2(102,23,"Bhagya");
-    cout << e1 << endl; //101 22 Athira
-    cout << e2 << endl; //102 23 Bhagya
-    e1.swap(e2);
-    cout << e1 << endl; //102 23 Bhagya
-    cout << e2 << endl; //101 22 Athira
-    */
 
-#include<iostream>
-#include <algorithm>
+#include <iostream>
+#include <cstring>
 using namespace std;
-#include<string>
 
-class Employee{
-public:    
+class Employee
+{
+private:
     int id;
     int age;
     string name;
+
 public:
-    Employee(int v_id, int v_age,string v_name):id(v_id),age(v_age),name(v_name){
+    void swp(Employee &other)
+    {
+        int tempID = this->id;
+        this->id = other.id;
+        other.id = tempID;
+
+        int tempAge = this->age;
+        this->age = other.age;
+        other.age = tempAge;
+
+        string tempName = this->name;
+        this->name = other.name;
+        other.name = tempName;
     }
 
-    friend ostream& operator << (ostream& os, Employee&);
+    Employee(int id, int age, string name);
+
+    friend ostream &operator<<(ostream &out, Employee &employee);
 };
 
-ostream& operator << (ostream& os, Employee& obj){
-    os << obj.id <<" " << obj.age << " "<< obj.name;
-    return os;
-}
-
-int main(){
-    Employee e1(101,22,"Athira"),e2(102,23,"Bhagya");
-    cout << "e1: " << e1 << endl;
-    cout << "e2: " <<  e2 << endl;
-    std::swap(e1,e2);
-    cout << "after swap"<< endl;
-    cout << "e1: " <<  e1 << endl;
-    cout << "e2: " <<  e2 << endl;
+int main()
+{
+    Employee e1(101, 22, "Athira"), e2(102, 23, "Bhagya");
+    cout << e1 << endl
+         << e2 << endl;
+    e1.swp(e2);
+    cout << e1 << endl
+         << e2 << endl;
 
     return 0;
+}
+
+Employee::Employee(int id, int age, string name) : id(id), age(age), name(name) {}
+ostream &operator<<(ostream &out, Employee &employee)
+{
+    out << "ID: " << employee.id << ", name: " << employee.name << ", age: " << employee.age;
+    return out;
 }
